7ffe2459a48b03926c7a08edbd8146c5c62b5d5e
AlisonGonzalez666 <112740141+AlisonGonzalez666@users.noreply.github.com>
2022-09-02 21:03:18 -0600

    Add files via upload

---
+++ ej2.js
@@ -0,0 +1,21 @@
+
+style{
+    table,td{border:1px inset;
+    border-color: rgb(0, 185, 200);
+    border-collapse:collapse;
+    text-align: center;
+}
+
+
+function calculaPrecioTotal(precio) {
+  var libros = 1.16;
+  var cuadernos = 10;
+  var plumas = 10;
+  var precioTotal = ( libros* cuadernos* plumas ) + total;
+}
+    
+function (guardar){
+    var libros=document.write(document.getElementById("libros");
+    var cuadernos=document.write(document.getElementById("cuadernos");
+    var plumas=document.wirte(document.getElementById("plumas");
+}
